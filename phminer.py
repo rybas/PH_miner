3c15ed12b1b217e4100c4fb816bd2e75ad7e3d0c
bibi_rg <bibi_rg@172.19.150.238>
2018-04-20 13:11:37 +0200

    fixed issues #1 and #2

--- phminer.py
+++ phminer.py
@@ -1,19 +1,33 @@
 import os
-
+import csv
 import yaml
+import io
 from ph_py import ProductHuntClient
 from ph_py.error import ProductHuntError
 
 config_file = 'credentials.yml'
 
-
 def run(key, secret, uri, token):
     phc = ProductHuntClient(key, secret, uri, token)
-
     # Example request
     try:
         for post in phc.get_todays_posts():
-            print(post.name)
+            name=post.name
+            tagline=post.tagline
+            urlLogo=post.screenshot_url
+            urlProduct=post.redirect_url
+            votes=post.votes_count
+            coments=post.comments_count
+            print('name: ', name)
+            print('tagline', tagline)
+            print('hunter:', post.user.name)
+            print(post.current_user)
+            with io.open('popular_YYYY-mm-dd.csv', 'w',newline='', encoding="utf-8") as csvfile:
+                spamwriter = csv.writer(csvfile, delimiter=' ',
+                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                spamwriter.writerow([name,tagline,urlLogo,urlProduct,votes,coments])
+
+
     except ProductHuntError as e:
         print(e.error_message)
         print(e.status_code)
